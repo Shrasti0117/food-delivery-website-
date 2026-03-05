21529951e914cf729833f4688175309f55fb1a88
shrasti gupta <guptashrasti25@gmail.com>
2026-03-05 09:22:08 +0530

    Added CORS for frontend connection

--- fontened/foodwebsite/src/context/StoreContext.jsx
+++ fontened/foodwebsite/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     
-    const url = "http://localhost:4000";
+    const url = "https://food-delivery-website-icc7.onrender.com";
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
